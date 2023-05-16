ce601cb6ebddf90de6267a4722b5c53b6d689385
Demilade362 <ademolademilade362@gmail.com>
2023-05-16 02:39:36 +0100

    Move Script to App.js file

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
     foo = $('#typewriter');
     foo.typer(['HTML, CSS, JAVASCRIPT, JQUERY', 'PHP, MYSQL, LARAVEL']);
+    wow = new WOW(
+        {
+            animateClass: 'animated',
+            offset: 100,
+            callback: function (box) {
+                console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+            }
+        }
+    );
+    wow.init();
 });
